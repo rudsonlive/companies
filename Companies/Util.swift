0ac224dea8410df72afbf528a1fc718eecad50d7
Rudson Lima <rudsonlive@gmail.com>
2016-07-17 16:09:54 -0300

    Add comments

--- Companies/Util.swift
+++ Companies/Util.swift
@@ -10,20 +10,24 @@ import Foundation
 class Util {
     
     struct Identifier {
+        // Meus identificadores
         static let webSiteView = "WebSiteView"
         static let mainCell = "MainCell"
     }
     
     struct Title {
+        // Meus títulos
         static let companies = "Companies"
         static let detail = "Detail"
     }
     
     struct BarButtonItemTitle {
+        // Meus títulos BarButtonItemTitle
         static let site = "Site"
     }
 
     struct FontName {
+        // Minhas fontes
         static let helveticaNeue = "Helvetica Neue"
     }
 }
